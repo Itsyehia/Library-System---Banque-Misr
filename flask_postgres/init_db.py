4735970304700a036e83b731ce65d853d2eb5482
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2024-08-19 00:10:22 +0300

    Add return function

--- flask_postgres/init_db.py
+++ flask_postgres/init_db.py
@@ -2,16 +2,10 @@ import psycopg2
 
 try:
     # Connect to the PostgreSQL database
-    conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="54321", port="54321")
+    conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="root", port="5432")
 
     # Create a cursor object to interact with the database
     cur = conn.cursor()
-
-    # Create the 'course' table
-    cur.execute('''CREATE TABLE course (id SERIAL PRIMARY KEY, name VARCHAR(100))''')
-
-    # Insert a test record into the 'course' table
-    cur.execute('''INSERT INTO course (name) VALUES ('test')''')
 
     # Commit the changes to the database
     conn.commit()
@@ -30,7 +24,7 @@ finally:
 
 def get_books():
     try:
-        conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="54321", port="54321")
+        conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="root", port="5432")
         cur = conn.cursor()
         cur.execute('''SELECT name FROM book ''')
         books = cur.fetchall()
@@ -43,9 +37,10 @@ def get_books():
         if conn:
             conn.close()
 
+
 def borrow_book(book_name, user_id):
     try:
-        conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="54321", port="54321")
+        conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="root", port="5432")
         cur = conn.cursor()
         cur.execute('''SELECT userID FROM book WHERE name = %s''', (book_name,))
         result = cur.fetchone()
@@ -67,4 +62,25 @@ def borrow_book(book_name, user_id):
             conn.close()
 
 
+def return_book_to_library(book_name, user_id):
+    try:
+        conn = psycopg2.connect(database="BM Task", host="localhost", user="postgres", password="root", port="5432")
+        cur = conn.cursor()
+        cur.execute('''SELECT userID FROM book WHERE name = %s''', (book_name,))
+        result = cur.fetchone()
 
+        if result is None:
+            message = f"The book '{book_name}' does not exist."
+        elif result[0] == user_id:
+            cur.execute('''UPDATE book SET userID = NULL WHERE name = %s''', (book_name,))
+            conn.commit()
+            message = f"Book '{book_name}' has been returned successfully by user with ID {user_id}."
+        else:
+            message = f"The book '{book_name}' was not borrowed by user with ID {user_id}."
+        cur.close()
+        return message
+    except psycopg2.Error as e:
+        return f"An error occurred: {e}"
+    finally:
+        if conn:
+            conn.close()
