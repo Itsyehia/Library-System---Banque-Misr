6b9994f7e9762ec97f248b189d5a05e5b9ae5580
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2024-09-10 21:05:27 +0300

    Update variables.tf

--- infrastructure  terraform/simple-Infrastructure/variables.tf
+++ infrastructure  terraform/simple-Infrastructure/variables.tf
@@ -1,20 +1,20 @@
 # Variable for User ARNs
 variable "user_arns" {
-  description = "List of IAM User ARNs allowed to assume the role"
-  type        = list(string)
+  description = "List of IAM User ARNs allowed to assume the role"  # Description of what this variable is used for
+  type        = list(string)  # Type of the variable: a list of strings
   default     = [
-    "arn:aws:iam::637423483309:user/yehia",
-    "arn:aws:iam::637423483309:user/yehia-2",
-    "arn:aws:iam::637423483309:user/reem"
+    "arn:aws:iam::637423483309:user/yehia",    # ARN for user 'yehia'
+    "arn:aws:iam::637423483309:user/yehia-2",  # ARN for user 'yehia-2'
+    "arn:aws:iam::637423483309:user/reem"      # ARN for user 'reem'
   ]
 }
 
 # Tags Variable
 variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
+  description = "Tags to apply to resources"  # Description of what this variable is used for
+  type        = map(string)  # Type of the variable: a map of strings
   default     = {
-    Environment = "Production"
-    Team        = "Team3-project"
+    Environment = "Production"  # Tag for environment, e.g., Production, Development
+    Team        = "Team3-project"  # Tag for the team or project name
   }
 }
