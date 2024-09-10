728b65f7d0350b5ba17687ef9b8a909988e66935
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2024-09-10 21:01:31 +0300

    Update main.tf

--- infrastructure  terraform/simple-Infrastructure/main.tf
+++ infrastructure  terraform/simple-Infrastructure/main.tf
@@ -1,11 +1,16 @@
-
-
+# Configure the AWS provider
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2"  # AWS region where resources will be created
 }
 
+# Configure the Kubernetes provider
 provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  token                  = data.aws_eks_cluster_auth.main.token
+  # Endpoint URL of the EKS cluster to connect to
+  host = aws_eks_cluster.main.endpoint
+  
+  # Authentication token for accessing the EKS cluster
+  token = data.aws_eks_cluster_auth.main.token
+  
+  # Base64-decoded certificate authority data for the EKS cluster
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
 }
