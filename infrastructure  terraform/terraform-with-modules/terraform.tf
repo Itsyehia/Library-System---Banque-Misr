a59f38465e521cade0a5008f7cfba7df863ae651
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2024-09-10 21:13:09 +0300

    Update terraform.tf

--- infrastructure  terraform/terraform-with-modules/terraform.tf
+++ infrastructure  terraform/terraform-with-modules/terraform.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0"
-
+      source  = "hashicorp/aws"  # Source of the AWS provider
+      version = ">= 3.0"         # Minimum version required for the AWS provider
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = var.region  # Specifies the AWS region from the variable
 }
